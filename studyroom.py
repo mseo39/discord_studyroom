77aff7501a9c14aa25f7d18ff9cf7eb213b394ce
mseo39 <a01063294905@gmail.com>
2024-04-24 23:47:03 +0900

    add 현재 공부시간 확인

--- studyroom.py
+++ studyroom.py
@@ -54,5 +54,39 @@ async def end(ctx):
         await ctx.send(f"{ctx.author.name}님, 공부를 종료했습니다. 공부한 시간: {hours:02d}:{minutes:02d}:{seconds:02d}")
     else:
         await ctx.send("공부를 시작하지 않았습니다.")
+
+# !ing 명령어 : 현재까지 공부 시간 확인
+@bot.command(name='ing')
+async def end(ctx):
+    
+    # 해당 사용자가 없다면
+    if ctx.author.id not in total_time:
+        await ctx.send("힘차게 공부를 시작해보세요!")
+    else:
+        # 공부를 하는 중이라면
+        if ctx.author.id in start_time:
+            # 그전 총시간이랑 현재 공부시간을 합해줘서 계산
+            start_t = start_time[ctx.author.id]
+            end_t = datetime.datetime.now()
+            tmp= total_time[ctx.author.id] + (end_t - start_t).total_seconds()
+            
+            #시분초 계산
+            total_seconds = int(tmp)
+            hours = total_seconds // 3600
+            minutes = (total_seconds % 3600) // 60
+            seconds = total_seconds % 60 
+            
+            await ctx.send(f"{ctx.author.name}님, 현재 공부시간: {hours:02d}:{minutes:02d}:{seconds:02d}")
+        else:
+            # 공부 중이 아니라면
+            
+            #시분초 계산
+            total_seconds = int(total_time[ctx.author.id])
+            hours = total_seconds // 3600
+            minutes = (total_seconds % 3600) // 60
+            seconds = total_seconds % 60 
+            
+            await ctx.send(f"{ctx.author.name}님, 현재 공부시간: {hours:02d}:{minutes:02d}:{seconds:02d}")
+        
  
 bot.run(Token)
